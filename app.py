4fe6f5e08bd54885e357e3e55d0639afc5c221d3
chaitanya-949 <chaitanyatulluri949@gmail.com>
2024-04-16 00:08:20 +0530

    final commit

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def predict_font(image):
 
     # Check confidence if within range
     confidence = prediction[0][predicted_font_index]  # Assuming first element is class probabilities
-    if confidence < 0.9345:  # Adjust this threshold based on your model's performance
+    if confidence < 0.9609:  # Adjust this threshold based on your model's performance
         predicted_font = "Font not found "
 
     return predicted_font
